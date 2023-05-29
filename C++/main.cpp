77defc44b2086caac8d4a363a6db91ebc64bfb80
Ricky <rickyzoe0722@gmail.com>
2023-05-29 16:19:06 +0800

    update some mark note.

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int size) {
+void sort(int arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
-        // 每一輪比較的次數.
+        // 每一回合的比對.
         for (int j = 0; j < size - i - 1; j++) {
-            // 如果相鄰元素順序錯誤，則交換它們.
+            // 檢查並調整交換相鄰的元素的順序.
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -19,13 +19,16 @@ int main() {
     int arr[] = {5, 3, 8, 2, 1};
     int size = sizeof(arr) / sizeof(arr[0]);
 
+    // 原始數列.
     cout << "original sequence: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
 
-    bubbleSort(arr, size);
+    // 泡沫排列.
+    sort(arr, size);
 
+    // 排序後數列.
     cout << "\nsorted sequence: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
